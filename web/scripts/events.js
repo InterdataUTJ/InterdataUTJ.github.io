65004581a0b641c5456d38ada6f7afa1e43a6cc8
IsmaCortGtz <ismacortgtz@gmail.com>
2024-03-06 10:07:07 -0600

    Screen size navbar stills showed bug fixed

--- web/scripts/events.js
+++ web/scripts/events.js
@@ -1,10 +1,11 @@
 // We need to add events again after the page is reloaded with view transitions.
 function addEvents() {
+  window.screen.orientation.addEventListener("change", hideNav);
+  window.addEventListener('resize', hideNav);
+  document.querySelector('main').addEventListener('click', hideNav);
   document.querySelector('img.menu-button-icon').addEventListener('click', () => {
     document.querySelector('nav').classList.toggle('nav-show');
   });
-
-  document.querySelector('main').addEventListener('click', hideNav);
 }
 
 function hideNav() {
